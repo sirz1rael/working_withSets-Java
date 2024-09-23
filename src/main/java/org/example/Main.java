c5149651cab2c3fb26d8017739df65e024bfef67
sirz1rael <sir.t0gga@gmail.com>
2024-09-23 09:20:58 +0300

    MergerSet class is ready

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,11 +8,11 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) throws Exception {
         Set<Integer> set1 = Set.of(1, 2, 3, 4, 5, 6);
-        Set<Integer> set2 = Set.of(4, 6, 3, 1);
+        Set<Integer> set2 = Set.of(12, 2, 3, 10, 7);
 
-        SetIntersectionChecker sic = new SetIntersectionChecker();
+        SetMerger sm = new SetMerger();
 
-        System.out.println(sic.isIntersecting(set1, set2));
+        System.out.println(sm.mergeSets(set1, set2));
 
     }
 }
--- src/main/java/org/example/tasks/SetMerger.java
+++ src/main/java/org/example/tasks/SetMerger.java
@@ -7,10 +7,15 @@ public class SetMerger {
     public Set<Integer> mergeSets(Set<Integer> set1, Set<Integer> set2) throws Exception {
         if (set1.isEmpty() || set2.isEmpty()) throw new Exception("One of arguments is empty :(");
 
+        UniqueNumberExtractor une = new UniqueNumberExtractor();
+
+        Set<Integer> unique1 = une.extractUniqueNumbers(set1.stream().toList());
+        Set<Integer> unique2 = une.extractUniqueNumbers(set2.stream().toList());
+
         Set<Integer> mergedSet = new HashSet<>();
 
-        /* TODO: Do it, using
-            the UniqueNumbersExtractor class. */
+        mergedSet.addAll(unique1);
+        mergedSet.addAll(unique2);
 
         return mergedSet;
     }
